29244f3fab9c30c898c19f877d0a23ac87de088e
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-04-01 09:19:17 -0500

    do a nil check instead of a guard let to remove the warning

--- EssentialFeed/Sources/EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/Sources/EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -18,7 +18,7 @@ public final class RemoteFeedLoader: FeedLoader {
 
     public func load(completion: @escaping (RemoteFeedLoader.Result) -> Void = { _ in }) {
         client.get(from: url) { [weak self] in
-            guard let self else { return }
+            guard self != nil else { return }
             switch $0 {
             case .success(let (data, response)):
                 completion(FeedItemsMapper.map(data, from: response))
