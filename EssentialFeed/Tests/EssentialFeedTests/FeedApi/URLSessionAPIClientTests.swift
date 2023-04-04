6feeff1256926272393fd43c4ee7947911f95e2c
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-04-03 20:22:17 -0500

    Finish URL loading when observing requests
    This ensures all requests are finished before the test method returns. This prevents data races with threads living longer than there test method that initiated them

--- EssentialFeed/Tests/EssentialFeedTests/FeedApi/URLSessionAPIClientTests.swift
+++ EssentialFeed/Tests/EssentialFeedTests/FeedApi/URLSessionAPIClientTests.swift
@@ -171,7 +171,6 @@ final class URLSessionAPIClientTests: XCTestCase {
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
             return true
         }
 
@@ -180,6 +179,11 @@ final class URLSessionAPIClientTests: XCTestCase {
         }
 
         override func startLoading() {
+            if let observer = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return observer(request)
+            }
+            
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
