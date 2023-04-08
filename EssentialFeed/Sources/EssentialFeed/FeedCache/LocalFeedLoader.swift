f289921d38d1fe2dd865ccad73ae4f5914634cfc
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-04-08 10:55:27 -0500

    clean up

--- EssentialFeed/Sources/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/Sources/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -14,11 +14,14 @@ public final class LocalFeedLoader: FeedLoader {
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
             guard self != nil else { return }
+
             switch result {
             case let .failure(error):
                 completion(.failure(error))
+        
             case let .found(feed: images, timeStamp: date) where Self.validate(date):
                     completion(.success(images.toFeedImage()))
+
             case .found, .empty:
                 completion(.success([]))
             }
@@ -34,10 +37,11 @@ public extension LocalFeedLoader {
     func save(_ items: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] error in
             guard let self else { return }
-            if error == nil {
-                self.cache(items, with: completion)
+
+            if let cacheDeletionError = error{
+                completion(cacheDeletionError)
             } else {
-                completion(error)
+                self.cache(items, with: completion)
             }
         }
     }
@@ -49,11 +53,14 @@ public extension LocalFeedLoader {
     func validateCache() {
         store.retrieve { [weak self]  result in
             guard let self else { return }
+
             switch result {
             case .failure:
                 self.store.deleteCachedFeed { _ in }
+
             case let .found(feed: _, timeStamp: date) where !Self.validate(date):
                 self.store.deleteCachedFeed { _ in }
+
             case .found, .empty:
                 break
             }
@@ -76,9 +83,15 @@ private extension LocalFeedLoader {
     
     static func validate(_ timeStamp: Date) -> Bool {
         let calendar = Calendar(identifier: .gregorian)
-        guard let cacheExpiration = calendar.date(byAdding: .day, value: -MAX_CACHE_AGE_IN_DAYS, to: Date()) else {
+        
+        guard let cacheExpiration = calendar.date(
+            byAdding: .day,
+            value: -MAX_CACHE_AGE_IN_DAYS,
+            to: Date()
+        ) else {
             return false
         }
+
         return timeStamp > cacheExpiration
     }
 }
