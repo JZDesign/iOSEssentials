988c62e7979bdaabb2d91330741006bf2903ecd9
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-04-14 21:54:38 -0500

    remove bailable for now

--- EssentialFeed/Tests/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/Tests/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -2,7 +2,7 @@ import XCTest
 import EssentialFeed
 import EssentialFeedAPITestUtilities
 
-final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
+final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_retrieve_deliversEmptyWhenCacheIsEmpty() {
         assertThat_Retrieve_DeliversEmptyOnEmptyCache(on: makeSUT())
@@ -20,38 +20,12 @@ final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
         assertThat_Retrieve_HasNoSideEffectsOnNonEmptyCache(on: makeSUT())
     }
     
-    func test_retrieve_deliversFailureOnRetrievalError() {
-//        let url = testSpecificStoreURL
-//        let sut = makeSUT(storeURL: url)
-//        try! "invalid data".write(to: url, atomically: false, encoding: .utf8)
-//        expect(sut, toRetrieve: .failure(anyNSError()))
-    }
-    
-    func test_retrieve_hasNoSideEffectsOnFailure () {
-//        let url = testSpecificStoreURL
-//        let sut = makeSUT(storeURL: url)
-//        try! "invalid data".write(to: url, atomically: false, encoding: .utf8)
-//        expect(sut, toRetrieve: .failure(anyNSError()), times: 2)
-    }
-    
     // MARK: - INSERT
 
     func test_insert_overridesPreviouslyCachedValues() {
         assertThat_Insert_OverridesPreviouslyCachedValues(on: makeSUT())
     }
     
-    func test_insert_deliversErrorOnInsertionError() {
-//        let invalidURL = URL(string: "invalid://store-url")!
-//        let sut = makeSUT(storeURL: invalidURL)
-//        assertThat_Insert_DeliversErrorOnInsertionError(on: sut)
-    }
-    
-    func test_insert_hasNoSideEffectsOnInsertionError() {
-//        let invalidURL = URL(string: "invalid://store-url")!
-//        let sut = makeSUT(storeURL: invalidURL)
-//        assertThat_Insert_HasNoSideEffectsOnInsertionError(on: sut)
-    }
-
     func test_insert_doesNotDeliverErrorOnEmptyCache() {
         assertThat_Insert_DoesNotDeliverErrorOnEmptyCache(on: makeSUT())
     }
@@ -69,19 +43,6 @@ final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
     func test_delete_emptiesPreviouslyInsertedCache() {
         assertThat_Delete_EmptiesPreviouslyInsertedCache(on: makeSUT())
     }
-    
-    func test_delete_deliversErrorOnDeletionError() {
-//        let noPermissionsToDelete = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-//        let sut = makeSUT(storeURL: noPermissionsToDelete)
-//
-//        assertThat_Delete_DeliversErrorOnDeletionError(on: sut)
-    }
-    
-     func test_delete_hasNoSideEffectsErrorOnDeletionError() {
-//         let noPermissionsToDelete = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-//         let sut = makeSUT(storeURL: noPermissionsToDelete)
-//         assertThat_Delete_HasNoSideEffectsErrorOnDeletionError(on: sut)
-     }
     
     func test_delete_doesNotDeliverErrorOnEmptyCache() {
         assertThat_Delete_DoesNotDeliverErrorOnEmptyCache(on: makeSUT())
